b087299f0f9a131886ecb18ca1e0e49dae017863
Emil Mescheryakov <emil.sam12@gmail.com>
2025-03-25 17:37:47 +0300

    v1.0 App with localization

---
+++ AllUnitConverter/AllUnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllUnitConverterApp.swift
+//  AllUnitConverter
+//
+//  Created by Emil Mescheryakov on 25.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct AllUnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
